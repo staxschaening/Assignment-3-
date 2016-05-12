893772e7ee7074f1a1d19a0afea47aaac111524e
stefan schaaening <staxschaening@gmail.com>
2016-05-13 01:54:11 +1000

    2 enemys have been added with no collisions.

--- Assignmnent/Source.cpp
+++ Assignmnent/Source.cpp
@@ -3,9 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-void Inputer(sf::Sprite &player);
-
-
+void Inputer(sf::Sprite & player);
 
 
 
@@ -24,15 +22,40 @@ int main()
 {
 	//this is the window screen.
 	sf::RenderWindow window(sf::VideoMode(800, 800), "GreenDude Explores");
+
+	//Test of Rendering Shapes.
+
 	//sf::CircleShape shape(100.f);
-	//shape.setFillColor(sf::Color::Green);
-	
+	//shape.setPosition(250, 200);
+	//shape.setFillColor(sf::Color::Blue);
+	//shape.setScale(10, 10);
+
+
+
+
+
 	//VIEW STUFF
 	sf::View view;
 	int myX = 0;
 
+
+
+
+
 	//
-	//Loading textures, sprites, walls, player, goes here
+	//Loading textures, sprites, walls, player & Enemys
+
+	//loading Enemy1
+	sf::Texture Reddudebluetx;
+	Reddudebluetx.loadFromFile("Enemy.png");
+	sf::Sprite Reddudebluespr(Reddudebluetx);
+	Reddudebluespr.setPosition(4500, 400);
+
+	//loading Enemy2
+	sf::Texture Yellowdudebluetx;
+	Yellowdudebluetx.loadFromFile("Yellowen.png");
+	sf::Sprite Yellowdudebluespr(Yellowdudebluetx);
+	Yellowdudebluespr.setPosition(1500, 400);
 
 
 	//loading greendude.
@@ -42,180 +65,166 @@ int main()
 	Greendudespr.setPosition(100, 100);
 
 	// loading texture wall, map
-
+	// looping and creating more wallbricks
 	sf::Texture wallBrickT;
-	wallBrickT.loadFromFile( "WallBrick.png");
+	wallBrickT.loadFromFile("WallBrick.png");
 	sf::Sprite wallBrickspr[132];
 
 	for (int i = 0; i < 130; i++) {
-	
-		
-		
-			//starting top bar
-			wallBrickspr[i].setTexture(wallBrickT);
-			wallBrickspr[0].setPosition(i * 0, 0);
-			wallBrickspr[1].setPosition(i * 1, 0);
-			wallBrickspr[2].setPosition(i * 2, 0);
-			wallBrickspr[3].setPosition(i * 3, 0);
-			wallBrickspr[4].setPosition(i * 4, 0);
-			wallBrickspr[5].setPosition(i * 5, 0);
-			wallBrickspr[6].setPosition(i * 6, 0);
-			wallBrickspr[7].setPosition(i * 7, 0);
-			wallBrickspr[8].setPosition(i * 8, 0);
-			//starting opening bar
-			wallBrickspr[9].setPosition(i * 9, 0);
-			wallBrickspr[10].setPosition(i * 9, 0);
-			wallBrickspr[11].setPosition(i * 9, 50);
-			wallBrickspr[12].setPosition(i * 9, 100);
-			wallBrickspr[13].setPosition(i * 9, 150);
-			wallBrickspr[14].setPosition(i * 9, 200);
-			wallBrickspr[15].setPosition(i * 9, 250);
-			wallBrickspr[16].setPosition(i * 9, 550);
-			wallBrickspr[17].setPosition(i * 9, 600);
-			wallBrickspr[18].setPosition(i * 9, 650);
-			wallBrickspr[19].setPosition(i * 9, 700);
-			wallBrickspr[20].setPosition(i * 9, 750);
 
-			//starting bottom bar
-			wallBrickspr[21].setPosition(i * 8, 750);
-			wallBrickspr[22].setPosition(i * 7, 750);
-			wallBrickspr[23].setPosition(i * 6, 750);
-			wallBrickspr[24].setPosition(i * 5, 750);
-			wallBrickspr[25].setPosition(i * 4, 750);
-			wallBrickspr[26].setPosition(i * 3, 750);
-			wallBrickspr[27].setPosition(i * 2, 750);
-			wallBrickspr[28].setPosition(i * 1, 750);
-			wallBrickspr[29].setPosition(i * 0, 750);
+		//starting top bar
+		wallBrickspr[i].setTexture(wallBrickT);
+		wallBrickspr[0].setPosition(i * 0, 0);
+		wallBrickspr[1].setPosition(i * 1, 0);
+		wallBrickspr[2].setPosition(i * 2, 0);
+		wallBrickspr[3].setPosition(i * 3, 0);
+		wallBrickspr[4].setPosition(i * 4, 0);
+		wallBrickspr[5].setPosition(i * 5, 0);
+		wallBrickspr[6].setPosition(i * 6, 0);
+		wallBrickspr[7].setPosition(i * 7, 0);
+		wallBrickspr[8].setPosition(i * 8, 0);
+		//starting opening bar
+		wallBrickspr[9].setPosition(i * 9, 0);
+		wallBrickspr[10].setPosition(i * 9, 0);
+		wallBrickspr[11].setPosition(i * 9, 50);
+		wallBrickspr[12].setPosition(i * 9, 100);
+		wallBrickspr[13].setPosition(i * 9, 150);
+		wallBrickspr[14].setPosition(i * 9, 200);
+		wallBrickspr[15].setPosition(i * 9, 250);
+		wallBrickspr[16].setPosition(i * 9, 550);
+		wallBrickspr[17].setPosition(i * 9, 600);
+		wallBrickspr[18].setPosition(i * 9, 650);
+		wallBrickspr[19].setPosition(i * 9, 700);
+		wallBrickspr[20].setPosition(i * 9, 750);
 
-			//starting close off bar
+		//starting bottom bar
+		wallBrickspr[21].setPosition(i * 8, 750);
+		wallBrickspr[22].setPosition(i * 7, 750);
+		wallBrickspr[23].setPosition(i * 6, 750);
+		wallBrickspr[24].setPosition(i * 5, 750);
+		wallBrickspr[25].setPosition(i * 4, 750);
+		wallBrickspr[26].setPosition(i * 3, 750);
+		wallBrickspr[27].setPosition(i * 2, 750);
+		wallBrickspr[28].setPosition(i * 1, 750);
+		wallBrickspr[29].setPosition(i * 0, 750);
 
-			wallBrickspr[30].setPosition(i * 0, 700);
-			wallBrickspr[31].setPosition(i * 0, 650);
-			wallBrickspr[32].setPosition(i * 0, 600);
-			wallBrickspr[33].setPosition(i * 0, 550);
-			wallBrickspr[34].setPosition(i * 0, 500);
-			wallBrickspr[35].setPosition(i * 0, 450);
-			wallBrickspr[36].setPosition(i * 0, 400);
-			wallBrickspr[37].setPosition(i * 0, 350);
-			wallBrickspr[38].setPosition(i * 0, 300);
-			wallBrickspr[39].setPosition(i * 0, 250);
-			wallBrickspr[40].setPosition(i * 0, 200);
-			wallBrickspr[41].setPosition(i * 0, 150);
-			wallBrickspr[42].setPosition(i * 0, 100);
-			wallBrickspr[43].setPosition(i * 0, 50);
+		//starting close off bar
+		wallBrickspr[30].setPosition(i * 0, 700);
+		wallBrickspr[31].setPosition(i * 0, 650);
+		wallBrickspr[32].setPosition(i * 0, 600);
+		wallBrickspr[33].setPosition(i * 0, 550);
+		wallBrickspr[34].setPosition(i * 0, 500);
+		wallBrickspr[35].setPosition(i * 0, 450);
+		wallBrickspr[36].setPosition(i * 0, 400);
+		wallBrickspr[37].setPosition(i * 0, 350);
+		wallBrickspr[38].setPosition(i * 0, 300);
+		wallBrickspr[39].setPosition(i * 0, 250);
+		wallBrickspr[40].setPosition(i * 0, 200);
+		wallBrickspr[41].setPosition(i * 0, 150);
+		wallBrickspr[42].setPosition(i * 0, 100);
+		wallBrickspr[43].setPosition(i * 0, 50);
 
+		//Bridge top 
+		wallBrickspr[87].setPosition(i * 10, 250);
+		wallBrickspr[88].setPosition(i * 11, 250);
+		wallBrickspr[89].setPosition(i * 12, 250);
+		wallBrickspr[90].setPosition(i * 13, 250);
+		wallBrickspr[91].setPosition(i * 14, 250);
+		wallBrickspr[92].setPosition(i * 15, 250);
+		wallBrickspr[93].setPosition(i * 16, 250);
+		wallBrickspr[94].setPosition(i * 17, 250);
+		wallBrickspr[95].setPosition(i * 18, 250);
+		wallBrickspr[96].setPosition(i * 19, 250);
+		wallBrickspr[97].setPosition(i * 20, 250);
+		wallBrickspr[98].setPosition(i * 21, 250);
+		wallBrickspr[99].setPosition(i * 22, 250);
+		wallBrickspr[100].setPosition(i * 23, 250);
+		wallBrickspr[101].setPosition(i * 24, 250);
+		wallBrickspr[102].setPosition(i * 25, 250);
+		wallBrickspr[103].setPosition(i * 26, 250);
+		wallBrickspr[104].setPosition(i * 27, 250);
+		wallBrickspr[105].setPosition(i * 28, 250);
+		wallBrickspr[106].setPosition(i * 29, 250);
 
-
-
-		
-
-			//Bridge top 
-
-			wallBrickspr[87].setPosition(i * 10,250);
-			wallBrickspr[88].setPosition(i * 11,250);
-			wallBrickspr[89].setPosition(i *12, 250);
-			wallBrickspr[90].setPosition(i *13, 250);
-			wallBrickspr[91].setPosition(i *14, 250);
-			wallBrickspr[92].setPosition(i *15, 250);
-			wallBrickspr[93].setPosition(i *16, 250);
-			wallBrickspr[94].setPosition(i *17, 250);
-			wallBrickspr[95].setPosition(i *18, 250);
-			wallBrickspr[96].setPosition(i *19, 250);
-			wallBrickspr[97].setPosition(i *20, 250);
-			wallBrickspr[98].setPosition(i *21, 250);
-			wallBrickspr[99].setPosition(i *22, 250);
-			wallBrickspr[100].setPosition(i *23, 250);
-			wallBrickspr[101].setPosition(i *24, 250);
-			wallBrickspr[102].setPosition(i *25, 250);
-			wallBrickspr[103].setPosition(i * 26, 250);
-			wallBrickspr[104].setPosition(i * 27, 250);
-			wallBrickspr[105].setPosition(i * 28, 250);
-			wallBrickspr[106].setPosition(i * 29, 250);
-			// bottom
-
-			wallBrickspr[107].setPosition(i * 10, 550);
-			wallBrickspr[108].setPosition(i * 11, 550);
-			wallBrickspr[109].setPosition(i * 12, 550);
-			wallBrickspr[110].setPosition(i * 13, 550);
-			wallBrickspr[112].setPosition(i * 14, 550);
-			wallBrickspr[113].setPosition(i * 15, 550);
-			wallBrickspr[114].setPosition(i * 16, 550);
-			wallBrickspr[115].setPosition(i * 17, 550);
-			wallBrickspr[116].setPosition(i * 18, 550);
-			wallBrickspr[117].setPosition(i * 19, 550);
-			wallBrickspr[118].setPosition(i * 20, 550);
-			wallBrickspr[119].setPosition(i * 21, 550);
-			wallBrickspr[120].setPosition(i * 22, 550);
-			wallBrickspr[121].setPosition(i * 23, 550);
-			wallBrickspr[122].setPosition(i * 24, 550);
-			wallBrickspr[123].setPosition(i * 25, 550);
-			wallBrickspr[124].setPosition(i * 26, 550);
-			wallBrickspr[125].setPosition(i * 27, 550);
-			wallBrickspr[126].setPosition(i * 28, 550);
-			wallBrickspr[127].setPosition(i * 29, 550);
-			
-			
-			//2nd Box
-			
-			wallBrickspr[44].setPosition(i * 30, 0);
-			wallBrickspr[45].setPosition(i * 31, 0);
-			wallBrickspr[46].setPosition(i * 32, 0);
-			wallBrickspr[47].setPosition(i * 33, 0);
-			wallBrickspr[48].setPosition(i * 34, 0);
-			wallBrickspr[49].setPosition(i * 35, 0);
-			wallBrickspr[50].setPosition(i * 36, 0);
-			wallBrickspr[51].setPosition(i * 37, 0);
-			wallBrickspr[52].setPosition(i * 38, 0);
-			// opneing bar
-			wallBrickspr[53].setPosition(i * 30, 0);
-			wallBrickspr[53].setPosition(i * 30, 0);
-			wallBrickspr[54].setPosition(i * 30, 50);
-			wallBrickspr[55].setPosition(i * 30, 100);
-			wallBrickspr[56].setPosition(i * 30, 150);
-			wallBrickspr[57].setPosition(i * 30, 200);
-			wallBrickspr[58].setPosition(i * 30, 250);
-			wallBrickspr[59].setPosition(i * 30, 550);
-			wallBrickspr[60].setPosition(i * 30, 600);
-			wallBrickspr[61].setPosition(i * 30, 650);
-			wallBrickspr[62].setPosition(i * 30, 700);
-			wallBrickspr[63].setPosition(i * 30, 750);
-
-			//starting bottom bar
-			wallBrickspr[64].setPosition(i * 38, 750);
-			wallBrickspr[65].setPosition(i * 37, 750);
-			wallBrickspr[66].setPosition(i * 36, 750);
-			wallBrickspr[67].setPosition(i * 35, 750);
-			wallBrickspr[68].setPosition(i * 34, 750);
-			wallBrickspr[69].setPosition(i * 33, 750);
-			wallBrickspr[70].setPosition(i * 32, 750);
-			wallBrickspr[71].setPosition(i * 31, 750);
-			wallBrickspr[72].setPosition(i *30, 750);
-
-			//starting close off bar
-
-			wallBrickspr[73].setPosition(i * 38, 700);
-			wallBrickspr[74].setPosition(i * 38, 650);
-			wallBrickspr[75].setPosition(i * 38, 600);
-			wallBrickspr[76].setPosition(i * 38, 550);
-			wallBrickspr[77].setPosition(i * 38, 500);
-			wallBrickspr[78].setPosition(i * 38, 450);
-			wallBrickspr[79].setPosition(i * 38, 400);
-			wallBrickspr[80].setPosition(i * 38, 350);
-			wallBrickspr[81].setPosition(i * 38, 300);
-			wallBrickspr[82].setPosition(i * 38, 250);
-			wallBrickspr[83].setPosition(i * 38, 200);
-			wallBrickspr[84].setPosition(i * 38, 150);
-			wallBrickspr[85].setPosition(i * 38, 100);
-			wallBrickspr[86].setPosition(i * 38, 50);
+		// bottom
+		wallBrickspr[107].setPosition(i * 10, 550);
+		wallBrickspr[108].setPosition(i * 11, 550);
+		wallBrickspr[109].setPosition(i * 12, 550);
+		wallBrickspr[110].setPosition(i * 13, 550);
+		wallBrickspr[112].setPosition(i * 14, 550);
+		wallBrickspr[113].setPosition(i * 15, 550);
+		wallBrickspr[114].setPosition(i * 16, 550);
+		wallBrickspr[115].setPosition(i * 17, 550);
+		wallBrickspr[116].setPosition(i * 18, 550);
+		wallBrickspr[117].setPosition(i * 19, 550);
+		wallBrickspr[118].setPosition(i * 20, 550);
+		wallBrickspr[119].setPosition(i * 21, 550);
+		wallBrickspr[120].setPosition(i * 22, 550);
+		wallBrickspr[121].setPosition(i * 23, 550);
+		wallBrickspr[122].setPosition(i * 24, 550);
+		wallBrickspr[123].setPosition(i * 25, 550);
+		wallBrickspr[124].setPosition(i * 26, 550);
+		wallBrickspr[125].setPosition(i * 27, 550);
+		wallBrickspr[126].setPosition(i * 28, 550);
+		wallBrickspr[127].setPosition(i * 29, 550);
 
 
+		//2nd Box
+		wallBrickspr[44].setPosition(i * 30, 0);
+		wallBrickspr[45].setPosition(i * 31, 0);
+		wallBrickspr[46].setPosition(i * 32, 0);
+		wallBrickspr[47].setPosition(i * 33, 0);
+		wallBrickspr[48].setPosition(i * 34, 0);
+		wallBrickspr[49].setPosition(i * 35, 0);
+		wallBrickspr[50].setPosition(i * 36, 0);
+		wallBrickspr[51].setPosition(i * 37, 0);
+		wallBrickspr[52].setPosition(i * 38, 0);
 
+		// opneing bar
+		wallBrickspr[53].setPosition(i * 30, 0);
+		wallBrickspr[53].setPosition(i * 30, 0);
+		wallBrickspr[54].setPosition(i * 30, 50);
+		wallBrickspr[55].setPosition(i * 30, 100);
+		wallBrickspr[56].setPosition(i * 30, 150);
+		wallBrickspr[57].setPosition(i * 30, 200);
+		wallBrickspr[58].setPosition(i * 30, 250);
+		wallBrickspr[59].setPosition(i * 30, 550);
+		wallBrickspr[60].setPosition(i * 30, 600);
+		wallBrickspr[61].setPosition(i * 30, 650);
+		wallBrickspr[62].setPosition(i * 30, 700);
+		wallBrickspr[63].setPosition(i * 30, 750);
 
+		//starting bottom bar
+		wallBrickspr[64].setPosition(i * 38, 750);
+		wallBrickspr[65].setPosition(i * 37, 750);
+		wallBrickspr[66].setPosition(i * 36, 750);
+		wallBrickspr[67].setPosition(i * 35, 750);
+		wallBrickspr[68].setPosition(i * 34, 750);
+		wallBrickspr[69].setPosition(i * 33, 750);
+		wallBrickspr[70].setPosition(i * 32, 750);
+		wallBrickspr[71].setPosition(i * 31, 750);
+		wallBrickspr[72].setPosition(i * 30, 750);
+
+		//starting close off bar
+		wallBrickspr[73].setPosition(i * 38, 700);
+		wallBrickspr[74].setPosition(i * 38, 650);
+		wallBrickspr[75].setPosition(i * 38, 600);
+		wallBrickspr[76].setPosition(i * 38, 550);
+		wallBrickspr[77].setPosition(i * 38, 500);
+		wallBrickspr[78].setPosition(i * 38, 450);
+		wallBrickspr[79].setPosition(i * 38, 400);
+		wallBrickspr[80].setPosition(i * 38, 350);
+		wallBrickspr[81].setPosition(i * 38, 300);
+		wallBrickspr[82].setPosition(i * 38, 250);
+		wallBrickspr[83].setPosition(i * 38, 200);
+		wallBrickspr[84].setPosition(i * 38, 150);
+		wallBrickspr[85].setPosition(i * 38, 100);
+		wallBrickspr[86].setPosition(i * 38, 50);
 
 	}
 
 
-	
+
 
 
 	//
@@ -240,9 +249,10 @@ int main()
 
 		// store player position for collision
 		sf::Vector2f playerWasHere = Greendudespr.getPosition();
+
 		// initialising player
 		Inputer(Greendudespr);
-		
+
 
 
 
@@ -262,7 +272,7 @@ int main()
 		}
 
 
-		
+
 		//view = window.getView();
 		view.setCenter(Greendudespr.getPosition().x, Greendudespr.getPosition().y);
 		window.setView(view);
@@ -281,6 +291,12 @@ int main()
 		//window.draw(shape);
 		//player being drawn in 
 		window.draw(Greendudespr);
+		//Enemy1 is drawn
+		window.draw(Reddudebluespr);
+		//Enemy2 is drawn
+		window.draw(Yellowdudebluespr);
+
+
 		//wall textures being drawn in
 		window.draw(wallBrickspr[0]);
 		window.draw(wallBrickspr[1]);
@@ -411,8 +427,8 @@ int main()
 		window.draw(wallBrickspr[125]);
 		window.draw(wallBrickspr[126]);
 		window.draw(wallBrickspr[127]);
-		
-		
+
+
 
 		window.display();
 	}
@@ -428,9 +444,9 @@ void Inputer(sf::Sprite &player) {
 	int boundry[4] = { 0,
 		0,
 		10000,
-		10000}; //TODO dynamically assign based on window size
+		10000 }; //TODO dynamically assign based on window size
 	sf::Vector2f pos = player.getPosition();
-	
+
 
 	int velocity = 1;
 	// GH input
