4096e467ccefe7fbacbe072f8f3b44719d40ef9b
stefan schaaening <staxschaening@gmail.com>
2016-05-13 13:11:33 +1000

    if player collides with enemy, resets players position, class of menu has been written and not implements and is not complete

--- Assignmnent/Source.cpp
+++ Assignmnent/Source.cpp
@@ -2,6 +2,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+//#include "Menu.h"
 
 
 
@@ -21,6 +22,63 @@ public:
 
 };
 
+/* part of the menu system could not implemented and isnt complete however the class diagram will clarify the process.
+void ();
+
+
+
+Menu menu(window.getSize().x, window.getSize().y);
+
+
+
+while (window.pollEvent(event))
+{
+switch (event.type)
+{
+case sf::Event::KeyReleased:
+switch (event.key.code)
+{
+case sf::Keyboard::Up:
+menu.MoveUp();
+break;
+
+case sf::Keyboard::Down:
+menu.MoveDown();
+break;
+
+case sf::Keyboard::Return:
+switch (menu.GetPressedItem())
+{
+case 0:
+std::cout << "Play button has been pressed" << std::endl;
+break;
+case 1:
+std::cout << "Option button has been pressed" << std::endl;
+break;
+case 2:
+window.close();
+break;
+}
+
+break;
+}
+
+break;
+case sf::Event::Closed:
+window.close();
+
+break;
+
+}
+}
+
+window.clear();
+
+menu.draw(window);
+
+window.display();
+}
+} */
 
 
 
@@ -233,7 +291,7 @@ int main()
 	}
 
 
-
+	//Menu menu(window.getSize().x, window.getSize().y);
 
 
 	//
@@ -249,7 +307,9 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
 		}
+
 
 
 
@@ -288,6 +348,8 @@ int main()
 
 			//If there was a collision, undo player movement;
 			Greendudespr.setPosition(playerWasHere);
+			//game restarts when player touches enemy
+			Greendudespr.setPosition(100, 100);
 		}
 
 		// collision for enemy 2
@@ -298,15 +360,31 @@ int main()
 
 			//If there was a collision, undo player movement;
 			Greendudespr.setPosition(playerWasHere);
-
+			//game restarts when player touches enemy
+			Greendudespr.setPosition(100, 100);
 		}
+
 
 		//view = window.getView();
 		view.setCenter(Greendudespr.getPosition().x, Greendudespr.getPosition().y);
 		window.setView(view);
 
-		//
-		//Logic to move things, etc goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -455,7 +533,7 @@ int main()
 		window.draw(wallBrickspr[126]);
 		window.draw(wallBrickspr[127]);
 
-
+		//menu.draw(window);
 
 		window.display();
 	}
